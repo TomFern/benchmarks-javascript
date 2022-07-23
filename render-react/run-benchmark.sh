7422f410c5f0c86cd9846353c3abd64c17c39176
tomas f <46322567+TomFern@users.noreply.github.com>
2022-07-23 11:19:59 -0300

    switch to oha for load testing

--- render-react/run-benchmark.sh
+++ render-react/run-benchmark.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+concurrency=50
+requests=2000000
+
 git clone https://github.com/oven-sh/bun.git
 cd bun/bench/react-hello-world
 
@@ -9,7 +12,7 @@ bun -v
 bun run react-hello-world.jsx &
 pid=$!
 sleep 1
-loadtest -k -c 8 -n 1000000 http://localhost:3000
+oha -c $concurrency -n $requests http://localhost:3000
 kill $pid
 
 # deno port 8080
@@ -18,7 +21,7 @@ deno --version
 deno run --allow-net react-hello-world.deno.jsx &
 pid=$!
 sleep 1
-loadtest -k -c 8 -n 1000000 http://localhost:8080
+oha -c $concurrency -n $requests http://localhost:8080
 kill $pid
 
 # node port 9080
@@ -27,7 +30,7 @@ node -v
 node react-hello-world.node.js &
 pid=$!
 sleep 1
-loadtest -k -c 8 -n 1000000 http://localhost:9080
+oha -c $concurrency -n $requests http://localhost:9080
 kill $pid
 
 rm -rf bun
